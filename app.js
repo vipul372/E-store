1654f17b691373345cc1386c19de66a49c30dd14
vipul372 <vipulpanchal372@gmail.com>
2021-08-23 13:25:03 +0530

    none

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use("/api", orderRoutes)
 app.use("/api", paymentBRoutes)
 
 //serve static asset if in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV == "production") {
   app.use(express.static(path.join(__dirname, "/client/build")))
   app.get("*", (req, res) => {
     res.sendFile(
